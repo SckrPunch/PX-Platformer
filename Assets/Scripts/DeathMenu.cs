367effb2a81f639a22be774a8bd202ad5f9a1b9a
SckrPunch <40640248+SckrPunch@users.noreply.github.com>
2020-10-19 02:51:35 -0400

    Combine json write with death menu

--- Assets/Scripts/DeathMenu.cs
+++ Assets/Scripts/DeathMenu.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using Unity;
+using Newtonsoft.Json;
 
 public class DeathMenu : MonoBehaviour
 {
     public int time_played;
     public int total_retries;
+    public List<DataJsonFormat> jsonObj_ = new List<DataJsonFormat>();
+    public int count_obj = 0;
 
     public void RestartLevel()
     {
@@ -34,6 +40,33 @@ public class DeathMenu : MonoBehaviour
         //data
         time_played = Mathf.RoundToInt(end_time - FieldManager.start_time);
         total_retries = FieldManager.retry_tutorial + FieldManager.retry_level;
+
+        //start json
+        jsonObj_.Add(new DataJsonFormat());
+        jsonObj_[count_obj].time_taken = time_played;
+        jsonObj_[count_obj].No_of_retries = total_retries;
+        jsonObj_[count_obj].retry_level = FieldManager.retry_level;
+        jsonObj_[count_obj].retry_tutorial = FieldManager.retry_tutorial;
+
+        count_obj++;
+
+        string json = JsonConvert.SerializeObject(jsonObj_);
+        //end json
+
+        WriteJSON(json);
+    }
+
+    public void WriteJSON(string json_)
+    {
+        string dir = Application.streamingAssetsPath + "/Data/JsonOutput";
+       
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        string output_file = dir + "/" + System.DateTime.Now.ToString("yyyy-MM-dd") + ".json";
+        File.WriteAllText(output_file, json_);
     }
 
     public void EndGame()
